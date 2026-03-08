d3b111ca017a47b3650d07ace163e0c0647aea5a
cheng yadong <chengyadong@chengdeiMac.local>
2026-03-08 21:17:48 +0800

    Disable DB SSL when ssl_mode is off

--- medusa-config.js
+++ medusa-config.js
@@ -2,9 +2,17 @@ const { loadEnv, defineConfig } = require("@medusajs/framework/utils")
 
 loadEnv(process.env.NODE_ENV || "development", process.cwd())
 
+const databaseUrl = process.env.DATABASE_URL
+const databaseDriverOptions = databaseUrl?.match(
+  /localhost|127\.0\.0\.1|ssl_mode=(disable|false)|sslmode=(disable)/i
+)
+  ? { connection: { ssl: false } }
+  : undefined
+
 module.exports = defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
+    databaseUrl,
+    databaseDriverOptions,
     http: {
       storeCors: process.env.STORE_CORS,
       adminCors: process.env.ADMIN_CORS,
